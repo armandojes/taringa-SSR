f8159f0d965c77263047b7a30a1d13767608b19c
Jesusmaster <armandodejesus678@gmail.com>
2019-05-23 13:26:17 -0500

    create routes

--- source/app/app.jsx
+++ source/app/app.jsx
@@ -1,15 +1,23 @@
 import React, { Fragment } from 'react';
 import Header from './components/header';
+import {Switch, Route} from 'react-router';
 import 'reset-css';
 import './app.css';
+
+//pages
+import Posts from './pages/posts';
+import Post from './pages/post';
+import User from './pages/user';
 
 function App (props) {
   return (
     <Fragment>
       <Header />
-      <div>
-        body page...
-      </div>
+      <Switch>
+        <Route path="/" exact component={Posts} />
+        <Route path="/post" exact component={Post} />
+        <Route path="/user" exact component={User} />
+      </Switch>
     </Fragment>
   )
 }
